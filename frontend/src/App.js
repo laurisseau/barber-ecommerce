842e0cd2565613a0b8c033de00d41608f183d33c
laurisseau <reso0208@gmail.com>
2023-04-13 10:02:27 -0400

    4/13/23

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import NavBarComp from "./components/NavbarComp";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
@@ -13,6 +14,35 @@ function App() {
           <Route path="/" element={<HomeScreen />} />
         </Routes>
       </main>
+
+      <footer
+        style={{ backgroundColor: "#252525", height: "170px", color: "white" }}
+      >
+        <Container
+          className="d-flex justify-content-md-between align-items-center flex-wrap justify-content-center"
+          style={{ height: "100%" }}
+        >
+          <div>
+            <h1 style={{ fontSize: "18px" }}>© 2023 Lawn Service</h1>
+            <p>Icon by www.wishforge.games on freeicons.io</p>
+            <a
+              style={{ color: "white" }}
+              href="https://www.instagram.com/yardi_kitchen/"
+            >
+              <i
+                className="fa-brands fa-instagram"
+                style={{ fontSize: "30px" }}
+              ></i>
+            </a>
+          </div>
+
+          <div>
+            <h1 style={{ fontSize: "18px" }}>
+              WEB DESIGN BY LAURISSEAU JOSEPH
+            </h1>
+          </div>
+        </Container>
+      </footer>
     </BrowserRouter>
   );
 }
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,29 @@
 import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 export default function HomeScreen() {
+  const ourProducts = { height: "400px", width: "300px", borderRadius: "5px" };
+
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 3,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+  };
+
   return (
     <>
       <header
@@ -27,29 +49,78 @@ export default function HomeScreen() {
         </Container>
       </header>
 
-      <Container
-        className="mt-5 mb-5 d-flex align-items-center flex-wrap justify-content-lg-between  justify-content-md-center red"
-        style={{ maxWidth: "1140px", height: "100%" }}
-        fluid
-      >
-        <div className="red" style={{ height: "100%", width: "500px" }}>
-          <h2>Save on Khalifa Kush, Love’s Oven, Bhang and Trekkers</h2>
-          <p>Limited Time Florida Deals</p>
-          <h5>
-            Puff like a Rock Star with KK at just $40. Get Baked with Big
-            savings on Bhang and Love’s Oven Edibles. Explore a little further
-            with $15 Trekkers.
-          </h5>
-          <Link to="/" className="body-btn">
-            Shop Now
-          </Link>
+      <Container style={{ maxWidth: "1140px", height: "100%" }} fluid>
+        <div className="mt-5 mb-3 d-flex align-items-center flex-wrap-reverse justify-content-lg-between  justify-content-md-center justify-content-sm-center">
+          <div
+            className="first-body-text mt-5 mb-5 m-3"
+            style={{ height: "100%", width: "500px" }}
+          >
+            <h2 style={{ fontWeight: "700", fontSize: "40px" }}>
+              Save on Khalifa Kush, Love’s Oven, Bhang and Trekkers
+            </h2>
+            <p>Limited Time Florida Deals</p>
+            <h5 style={{ fontWeight: "450" }}>
+              Puff like a Rock Star with KK at just $40. Get Baked with Big
+              savings on Bhang and Love’s Oven Edibles. Explore a little further
+              with $15 Trekkers.
+            </h5>
+            <button href="/" className="body-btn mt-4">
+              Shop Now
+            </button>
+          </div>
+
+          <img
+            alt="hair product"
+            className="responsive"
+            style={{ borderRadius: "100%", height: "350px", width: "400px" }}
+            src="https://images.unsplash.com/photo-1630082900894-edbd503588f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+          ></img>
         </div>
 
-        <img
-          className="red"
-          style={{ borderRadius: "100%", height: "500px", width: "500px" }}
-          src="https://images.unsplash.com/photo-1630082900894-edbd503588f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-        ></img>
+        <div className="text-center mb-5 " style={{ height: "500px" }}>
+          <h1 className="mb-5">Our Products</h1>
+
+          <Carousel
+            swipeable={true}
+            responsive={responsive}
+            infinite={true}
+            partialVisible={true}
+          >
+            <Link to="/">
+              <img
+                alt="product"
+                src="https://images.unsplash.com/photo-1610705267928-1b9f2fa7f1c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                className=""
+                style={ourProducts}
+              ></img>
+            </Link>
+            <Link to="/">
+              <img
+                alt="product"
+                src="https://images.unsplash.com/photo-1608248543803-ba4f8c70ae0b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1053&q=80"
+                className=""
+                style={ourProducts}
+              ></img>
+            </Link>
+
+            <Link to="/">
+              <img
+                alt="product"
+                src="https://images.unsplash.com/photo-1556228852-80b6e5eeff06?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+                className=""
+                style={ourProducts}
+              ></img>
+            </Link>
+            <Link to="/">
+              <img
+                alt="product"
+                src="https://images.unsplash.com/photo-1526947425960-945c6e72858f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                className=""
+                style={ourProducts}
+              ></img>
+            </Link>
+          </Carousel>
+        </div>
       </Container>
     </>
   );
